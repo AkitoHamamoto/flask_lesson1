6454f7a6c6f9522c7a883704245a02e4d974997c
AkitoHamamoto <akatsuki.blog.fun@gmail.com>
2021-01-22 19:17:55 +0900

    ルーティング, デバック

--- app.py
+++ app.py
@@ -6,6 +6,21 @@ app = Flask(__name__)
 def index():
   return '<h1>Hello World</h1>'
 
+@app.route('/hello')
+@app.route('/something')
+def hello():
+  return '<h2>my hello</h2>'
+
+@app.route('/post/<int:post_id>/<post_name>') #入力したURLによってページを変える
+def show_post(post_id, post_name):
+  # print(type(post_id))
+  return '{}: {}'.format(post_name, post_id)
+
+@app.route('/user/<string:user_name>/<int:user_no>')
+def show_user(user_name, user_no):
+  user_name_no = user_name + str(user_no)
+  return '<h1>{}</h1>'.format(user_name_no)
+
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
     
